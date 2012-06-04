52ab0550a8672d2d58adea008156440111f1ba4d
Eric Stratmann <estrat@cs.stanford.edu>
2012-06-04 13:21:35 -0700

    Add onAttached and OnCreated tab handlers

--- background.coffee
+++ background.coffee
@@ -4,6 +4,19 @@ class Browser
 
     addWindow: (window) ->
         @windows.push window
+
+    removeWindow: (window) ->
+        @windows.remove @windows.indexOf window
+
+    getWindowById: (id) ->
+        @windows.findFirst (window) ->
+            window.chromeWindow.id == id
+
+    getTabById: (id) ->
+        window = @windows.findFirst (window) ->
+            window.getTabById id
+        window.getTabById id
+
 class Window
     constructor: (chromeWindow) ->
         @chromeWindow = chromeWindow
@@ -11,13 +24,48 @@ class Window
 
     addTab: (tab) ->
         @tabs.push tab
+        tab.setWindow @
+
+    removeTab: (tab) ->
+        @tabs.remove @tabs.indexOf tab
+
+    getTabById: (id) ->
+        @tabs.findFirst (tab) ->
+            tab.chromeTab.id == id
 
 class Tab
     constructor: (chromeTab) ->
         @chromeTab = chromeTab
+        @window = null
+
+    setWindow: (window) ->
+        @window.removeTab @ if @window
+        @window = window
+
+Array.prototype.findFirst = (f) ->
+    ret = this.filter (i) -> f i
+    return ret[0] if ret.length > 0
+    null
+
+Array.prototype.remove = (i) ->
+    this[i] = this[this.length - 1]
+    delete this[this.length - 1]
+
+onTabCreated = (chromeTab) ->
+    window = browser.getWindowById chromeTab.windowId
+    tab = new Tab chromeTab
+    window.addTab tab
+
+onTabAttached = (tabId, attachInfo) ->
+    tab = browser.findTabBy tabId
+    window = browser.findWindow attackInfo.newWindowId
+    window.addTab tab
+
+chrome.tabs.onAttached.addListener onTabAttached
+chrome.tabs.onCreated.addListener onTabCreated
 
 browser = new Browser
- 
+
 chrome.windows.getAll populate: true, (chromeWindows) ->
     chromeWindows.forEach (chromeWindow) ->
         window = new Window chromeWindow
--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 (function() {
-  var Browser, Tab, Window, browser;
+  var Browser, Tab, Window, browser, onTabAttached, onTabCreated;
   Browser = function() {
     this.windows = [];
     return this;
@@ -7,18 +7,77 @@
   Browser.prototype.addWindow = function(window) {
     return this.windows.push(window);
   };
+  Browser.prototype.removeWindow = function(window) {
+    return this.windows.remove(this.windows.indexOf(window));
+  };
+  Browser.prototype.getWindowById = function(id) {
+    return this.windows.findFirst(function(window) {
+      return window.chromeWindow.id === id;
+    });
+  };
+  Browser.prototype.getTabById = function(id) {
+    var window;
+    window = this.windows.findFirst(function(window) {
+      return window.getTabById(id);
+    });
+    return window.getTabById(id);
+  };
   Window = function(chromeWindow) {
     this.chromeWindow = chromeWindow;
     this.tabs = [];
     return this;
   };
   Window.prototype.addTab = function(tab) {
-    return this.tabs.push(tab);
+    this.tabs.push(tab);
+    return tab.setWindow(this);
+  };
+  Window.prototype.removeTab = function(tab) {
+    return this.tabs.remove(this.tabs.indexOf(tab));
+  };
+  Window.prototype.getTabById = function(id) {
+    return this.tabs.findFirst(function(tab) {
+      return tab.chromeTab.id === id;
+    });
   };
   Tab = function(chromeTab) {
     this.chromeTab = chromeTab;
+    this.window = null;
     return this;
   };
+  Tab.prototype.setWindow = function(window) {
+    if (this.window) {
+      this.window.removeTab(this);
+    }
+    return (this.window = window);
+  };
+  Array.prototype.findFirst = function(f) {
+    var ret;
+    ret = this.filter(function(i) {
+      return f(i);
+    });
+    if (ret.length > 0) {
+      return ret[0];
+    }
+    return null;
+  };
+  Array.prototype.remove = function(i) {
+    this[i] = this[this.length - 1];
+    return delete this[this.length - 1];
+  };
+  onTabCreated = function(chromeTab) {
+    var tab, window;
+    window = browser.getWindowById(chromeTab.windowId);
+    tab = new Tab(chromeTab);
+    return window.addTab(tab);
+  };
+  onTabAttached = function(tabId, attachInfo) {
+    var tab, window;
+    tab = browser.findTabBy(tabId);
+    window = browser.findWindow(attackInfo.newWindowId);
+    return window.addTab(tab);
+  };
+  chrome.tabs.onAttached.addListener(onTabAttached);
+  chrome.tabs.onCreated.addListener(onTabCreated);
   browser = new Browser();
   chrome.windows.getAll({
     populate: true
